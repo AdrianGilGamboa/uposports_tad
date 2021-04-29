1e87fd0ea62b9a88fe43577dc8c320cbb11fb454
mamofu <63147643+mamofu@users.noreply.github.com>
2021-04-29 17:09:04 +0200

    Mis cosas

---
+++ uposports/src/main/java/manu/Empleado.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.epd_evaluable;
+
+/**
+ *
+ * @author manum
+ */
+public class Empleado {
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private String telefono;
+
+    public Empleado(String dni, String nombre, String apellidos, String telefono) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+    }
+
+   
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+     @Override
+    public String toString() {
+        return "Empleado{" + "dni=" + dni + ", nombre=" + nombre + ", apellidos=" + apellidos + ", telefono=" + telefono + '}';
+    }
+}
