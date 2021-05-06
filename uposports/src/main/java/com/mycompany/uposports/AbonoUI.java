dc18a3190253e46e182bfddefae2e2beda75f385
Adrián Gil Gamboa <57912630+AdrianGilGamboa@users.noreply.github.com>
2021-05-06 17:33:06 +0200

    .

--- uposports/src/main/java/com/mycompany/uposports/AbonoUI.java
+++ uposports/src/main/java/com/mycompany/uposports/AbonoUI.java
@@ -64,17 +64,17 @@ public class AbonoUI extends UI {
             getUI().getPage().setLocation("/Instalacion");//Accedemos a la entidad abono
         });
 
-        Button buttonEmpleados = new Button("Empleados", FontAwesome.BUILDING);//Botón para acceder a la entidad instalaciones
+        Button buttonEmpleados = new Button("Empleados", FontAwesome.GROUP);//Botón para acceder a la entidad instalaciones
         buttonEmpleados.addClickListener(e -> {//Acción del botón
             getUI().getPage().setLocation("/Empleado");//Accedemos a la entidad abono
         });
 
-        Button buttonMateriales = new Button("Materiales", FontAwesome.BUILDING);//Botón para acceder a la entidad instalaciones
+        Button buttonMateriales = new Button("Materiales", FontAwesome.ARCHIVE);//Botón para acceder a la entidad instalaciones
         buttonMateriales.addClickListener(e -> {//Acción del botón
             getUI().getPage().setLocation("/Material");//Accedemos a la entidad abono
         });
 
-        Button buttonReservas = new Button("Reservas", FontAwesome.BUILDING);//Botón para acceder a la entidad instalaciones
+        Button buttonReservas = new Button("Reservas", FontAwesome.CALENDAR);//Botón para acceder a la entidad instalaciones
         buttonReservas.addClickListener(e -> {//Acción del botón
             getUI().getPage().setLocation("/Material");//Accedemos a la entidad abono
         });
